53f1a788427fff777fa5801abb7d538cdc767ded
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-03-20 12:32:58 -0300

    function instead of string

--- public/hacked-pingbacks-0.1.js
+++ public/hacked-pingbacks-0.1.js
@@ -3,7 +3,8 @@ function hacked_pingbacks_success(itens) {
 	itens = itens.query.results["Result"];
 	for(var i = 1; i < itens.length; i++) {
 		item = itens[i];
-		$('#content').append("<li><a href=\"" + item["Url"] + "\">" + item["Title"] + "</a></li>");
+		var line = "<li><a href=\"" + item["Url"] + "\">" + item["Title"] + "</a></li>";
+		$('#content').append(line);
 	}
 	$('#content').append("</ul>");
 }
@@ -14,10 +15,13 @@ hacked_pingbacks = {
 			fill : function(options) {
 				callback_name = "hacked_pingbacks_success";
 				if(options) callback_name = options["callback"] || callback_name;
-				var uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20search.siteexplorer.inlinks%20where%20query%3D%22" + base_uri + "%22&format=json&callback=" + callback_name
-				$.ajax({
-					url: uri,
-					type: "GET",
+				var uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20search.siteexplorer.inlinks%20where%20query%3D%22" + base_uri + "%22&format=json&callback=?";
+				$.getJSON(uri, function(data) {
+					if(options && options["callback"]) {
+						options["callback"](data);
+					} else {
+						hacked_pingbacks_success(data);
+					}
 				});
 			}
 		};
