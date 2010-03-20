cda4cc40a0d9a4218c67d25bec954e219ef87b12
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-03-20 11:03:58 -0300

    runs on yahoo

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -3,11 +3,12 @@ require 'net/http'
 
 class DucksController < ApplicationController
   def index
-
-       url = URI.parse(params[:uri])
-       res = Net::HTTP.start(url.host, url.port) {|http|
-         http.get(url.path.empty? ? "/" : url.path)
-       }
+      uri = params[:uri]
+      uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20search.siteexplorer.inlinks%20where%20query%3D%22" + uri + "%22&format=json&callback=cbfunc"
+      url = URI.parse(params[:uri])
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.get(url.path.empty? ? "/" : url.path)
+      }
     render :text => res.body
   end
 end
